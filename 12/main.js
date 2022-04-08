739090e4375bdac3d90562a995944b6ab7a76a83
Gonzalo Blanco <gonzalo.br@outlook.com>
2022-04-08 22:20:48 +0200

    Day 12 A fixed. B to be tested

--- 12/main.js
+++ 12/main.js
@@ -1,5 +1,3 @@
-const { findSourceMap } = require('module');
-
 const input = require('fs').readFileSync('input', 'utf-8').split(/\r?\n/)
 
 const input_map = input.map(row => {
@@ -44,12 +42,12 @@ function getUnionsMap(input_map) {
 }
 
 
-function isRoadChecked(road, roads) {
+function isRoadChecked(road, checked_roads) {
 
     //console.log('this road', road)
-    //console.log('all roads', roads)
+    //console.log('checked roads', checked_roads)
 
-    for (let tmp_road of roads) {
+    for (let tmp_road of checked_roads) {
         //tmp_road = tmp_road.slice(0, tmp_road.length-1)
 
         if (JSON.stringify(tmp_road) == JSON.stringify(road)) {
@@ -60,11 +58,49 @@ function isRoadChecked(road, roads) {
     return false
 }
 
+function skipSmallCave(next_node, road) {
+    if (next_node.toLowerCase() == next_node) {
 
-function findWay(road, roads) {
+        //CASE A / CASE B
+        if (small_caves_just_once) {
+            //A: if in road -> cannot access it again
+            if (road.includes(next_node)) {
+                return true
+            }
+
+        } else {
+            //B: if 2 small caves visited -> cannot access it again
+            //if not 2 small visited yet -> allow to visit
+            let small_caves = road.filter(node =>  {
+                if ( (node == 'start') || (node == 'end') ) {
+                    return false //delete start and end
+                }
+                return (node.toLowerCase() == node) 
+            });
+
+            
+            if (small_caves.length == [...new Set(small_caves)].length) {
+                //not repeated small caves -> allow visit
+                return false
+            } else {
+                //if in road -> cannot access it again
+                if (road.includes(next_node)) {
+                    return true
+                }
+            }
+        }
+
+    }
+
+    return false
+}
+
+
+function findWay(road, roads, checked_roads) {
     let this_node = road[road.length-1]
 
     if (this_node == 'end') {
+        checked_roads.push(road)
         return road;
     }
 
@@ -77,56 +113,63 @@ function findWay(road, roads) {
         }
 
         //check if next node is lowercase
-        if (u_node.toLowerCase() == u_node) {
-            //if in road -> cannot access it again
-            if (road.includes(u_node)) {
-                continue
-            }
+        if (skipSmallCave(u_node, road)) {
+            continue
         }
 
         
-        //check if road for this u_node have been already saved
         let possible_road = [...road]
         possible_road.push(u_node)
-        if (isRoadChecked(possible_road, roads)) {
+        
+        //check if road for this u_node have been already saved
+        if (isRoadChecked(possible_road, checked_roads)) {
             //console.log('road checked')
             continue;
         }
         
-        let correct_road = findWay(possible_road, roads)
+        
+        let correct_road = findWay(possible_road, roads, checked_roads)
+        
         if (correct_road) {
             return correct_road
+        } else {
+            //all roads for this node checked
+            checked_roads.push(possible_road)
         }
     }
 
+
+    //Save this node (and road to get to it) as complete
     return false;
 }
 
 
 function testA() {
     let roads = []
+    let checked_roads = [];
 
     //do while no empty road
     let road = []
     do {
-        road = findWay(['start'], roads)
+        road = findWay(['start'], roads, checked_roads)
         if (road) {
-            console.log('correct road: ', road)
+            //console.log('correct road: ', road)
             roads.push(road)
         }
     } while (road)
 
-    console.log('\n\nfinal roads', roads)
+    //console.log('\n\nfinal roads', roads)
     return roads.length
 }
   
 
 function testB() {
-   
+   small_caves_just_once = false
+   return testA()
 }
 
-
-console.log("A: "+testA())
-//console.log("B: "+testB())
+let small_caves_just_once = true //A / B anwser
+//console.log("A: "+testA())
+console.log("B: "+testB())
 
 
